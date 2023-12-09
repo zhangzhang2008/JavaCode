723aa692ea325864f517a1ac03de15f52d25fe0d
zhangzhang2008 <81637448+zhangzhang2008@users.noreply.github.com>
2023-12-09 20:52:53 +0800

    mybatis自动生成代码提交

--- module01_base/src/main/java/com/tx/base/HelloWorld.java
+++ module01_base/src/main/java/com/tx/base/HelloWorld.java
@@ -1,7 +1,10 @@
 package com.tx.base;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 
 public class HelloWorld {
 
@@ -12,7 +15,12 @@ public class HelloWorld {
         // 单行注释
         System.out.println("hello world!");
 
-        /*List list = new ArrayList<>();*/
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.forEach(a -> System.out.println("==>" + a));
+
         test();
 
         System.out.println("111111");
@@ -24,47 +32,32 @@ public class HelloWorld {
         System.out.println("-------------------");
         System.out.println("123");
     }
-
-    public void method() {
-        System.out.println("method");
-    }
-
     public static void test() {
 
         String str = "test";
-        if (str != null) {
-
-        }
-
-        if (str == null) {
-
-        }
 
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
         }
 
-        String ss = new String("111");
+        String ss = "111";
         System.out.println("ss = " + ss);
-
-        System.out.printf("test", ss);
         System.out.println(ss);
 
-        Map map = new HashMap();
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "son");
+        System.out.println(map);
 
         test2();
     }
 
     private static void test2() {
-        String test = "zhangsan";
-        System.out.println(test);
-        System.out.println(test);
-        System.out.println(test);
+        String test = "hangman";
         System.out.println(test);
     }
 
     private static void method3(int a, int b) {
-        String strs = new String();
-        new String("hello");
+        String hello = "hello";
+        System.out.println(hello);
     }
 }
